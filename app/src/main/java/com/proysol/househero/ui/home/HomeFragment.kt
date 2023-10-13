78db46c84f08bc2716baa3e5c4dcd75e86235cb3
Renato Valer <valer9826@gmail.com>
2023-10-12 23:53:20 -0500

    Add ivProfilPic in Home

--- app/src/main/java/com/proysol/househero/ui/home/HomeFragment.kt
+++ app/src/main/java/com/proysol/househero/ui/home/HomeFragment.kt
@@ -1,29 +1,30 @@
 package com.proysol.househero.ui.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.proysol.househero.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.proysol.househero.databinding.FragmentHomeBinding
 import com.proysol.househero.ui.home.adapter.HomeAdapter
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import com.proysol.househero.R
 
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-    val binding get()= _binding
+    val binding get() = _binding
 
     private val viewModel: HomeViewModel by viewModels()
-    private lateinit var homeMenuAdapter:HomeAdapter
+    private lateinit var homeMenuAdapter: HomeAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -41,12 +42,24 @@ class HomeFragment : Fragment() {
     private fun initUI() {
         initMenuOptions()
         initObservers()
+        initViews()
+    }
+
+    private fun initViews() {
+        val options = RequestOptions().centerCrop().placeholder(R.drawable.margaret)
+
+        binding?.ivProfilePic?.let {
+            Glide.with(this)
+                .load(R.drawable.margaret)
+                .apply(options)
+                .into(it)
+        }
     }
 
     private fun initObservers() {
         lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED){
-                viewModel.menuOptions.collect{
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.menuOptions.collect {
                     homeMenuAdapter.update(it)
                 }
             }
